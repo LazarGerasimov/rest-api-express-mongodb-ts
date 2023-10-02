aaf2e0c75280521f8c36d782446c01b402d2decb
Laz <lazgerasimov@gmail.com>
2023-10-02 16:51:05 +0100

    create isAuthenticated middleware

---
+++ src/middlewares/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import { get, merge } from 'lodash';
+
+import { getUserBySessionToken } from '../services/userService';
+
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const sessionToken = req.cookies['Auth'];
+
+        if (!sessionToken) {
+            console.log("403 - Not Authorized");
+            return res.sendStatus(403);
+        }
+
+        const existingUser = await getUserBySessionToken(sessionToken);
+
+        if (!existingUser) {
+            console.log("Not authenticated");
+            return res.sendStatus(403);
+        }
+
+        merge(req, { identity: existingUser });
+        return next();
+        
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
