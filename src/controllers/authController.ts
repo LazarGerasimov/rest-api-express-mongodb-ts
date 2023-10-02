193c58808c5aa81d7a4b94de45c3929265c9630d
Laz <lazgerasimov@gmail.com>
2023-10-02 16:36:39 +0100

    complete login controller; test login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -29,7 +29,45 @@ export const register = async (req: express.Request, res: express.Response) => {
                 password: authentication(salt, password)
             }
         });
-        
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+};
+
+export const login = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            console.log("All fields must be filled.");
+            return res.sendStatus(400);
+        };
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if (!user) {
+            console.log("User does not exist.");
+            return res.sendStatus(400);
+        };
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password !== expectedHash) {
+            console.log("403 - Not Authorized")
+            return res.sendStatus(403);
+        }
+
+        const salt = randomizer();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('Auth:', user.authentication.sessionToken, { domain: 'localhost', path: '/' })
+
         return res.status(200).json(user).end();
 
     } catch (error) {
