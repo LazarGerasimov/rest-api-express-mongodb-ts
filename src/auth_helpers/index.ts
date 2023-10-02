48b152eaee6c3fa031c00f0f70ba13848517815d
Laz <lazgerasimov@gmail.com>
2023-10-02 11:47:38 +0100

    add authentication helpers

---
+++ src/auth_helpers/index.ts
@@ -0,0 +1,12 @@
+import crypto from 'crypto';
+
+require('dotenv').config();
+
+const secret = process.env.AUTH_SECRET; 
+
+export const randomizer = () => crypto.randomBytes(128).toString('base64');
+
+export const authentication = (salt: string, password: string) => {
+    return crypto.createHmac('sha256', [salt, password].join('/')).update(secret).digest('hex');
+};
+
