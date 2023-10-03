715c44884e67098235551ed8faa6b13cf97e0c20
Laz <lazgerasimov@gmail.com>
2023-10-03 17:20:02 +0100

    add isOwner route guard: - add isOwner middleware to make sure that only owners can delete their own accounts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -27,4 +27,26 @@ export const isAuthenticated = async (req: express.Request, res: express.Respons
         console.log(error);
         return res.sendStatus(400);
     }
+};
+
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+
+        if (!currentUserId) {
+            console.log("You are not authorized to do that.");
+            return res.sendStatus(403);
+        }
+
+        if (currentUserId.toString() !== id) {
+            console.log("You are not the owner");
+            return res.sendStatus(403);
+        }
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
